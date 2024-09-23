95964f0838a36b8a5a9a4ec7ffb5433fa6b24a91
Daron <darkenciel31@gmail.com>
2024-09-23 15:33:29 +0200

    Creation of the Connexion screen page and add of the redirection

---
+++ lib/Screens/ConnexionScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ConnexionScreen extends StatefulWidget {
+  const ConnexionScreen({super.key});
+
+  @override
+  State<ConnexionScreen> createState() => _ConnexionScreenState();
+}
+
+class _ConnexionScreenState extends State<ConnexionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("App"),
+        ),
+        body: const Center(
+          child: Text("Page de connexion"),
+        ),
+      ),
+    );
+  }
+}
