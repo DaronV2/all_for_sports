cb910965dbdcab75a6dab03891678616dadace42
Daron <darkenciel31@gmail.com>
2024-09-27 09:57:43 +0200

    Creation of connexionPage and create json with the id and the password

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AP3 - All4Sport (Semestre 3) - Programmation Mobile
+
+Vous avez besoin d'installer ceci avant d'éxecuter ce projet : flutter pub add json_annotation dev:build_runner dev:json_serializable
--- lib/Screens/ConnexionScreen.dart
+++ lib/Screens/ConnexionScreen.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:all_for_sports/Services/SerializeLogs.dart';
 import 'package:flutter/material.dart';
 
 class ConnexionScreen extends StatefulWidget {
@@ -8,25 +11,65 @@ class ConnexionScreen extends StatefulWidget {
 }
 
 class _ConnexionScreenState extends State<ConnexionScreen> {
+
+  bool _passwordHidden = true;
+
+  final TextEditingController _controllerId = TextEditingController();
+
+  final TextEditingController _controllerPassword = TextEditingController();
+
+  @override
+  void initState() {
+    _passwordHidden = true;
+    // TODO: implement initState
+    // super.initState();
+    // _controllerId.addListener(() {
+    //   final String textId = _controllerId.text.toLowerCase();
+    //   // print(textId);
+    // });
+    // _controllerPassword.addListener(() {
+    //   final String textPassword = _controllerPassword.text;
+    //   // print(textPassword);
+    // });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("App"),
+          title: const Center(
+            child: Text("App"),
+          ),
         ),
-        body: Stack(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.person),
-                hintText: "Entrez votre identifiant : ",
-                contentPadding: EdgeInsets.all(5),
-                border: OutlineInputBorder(),
-              ),
-            )
-          ]
-        ),
+        body: Column(children: [
+          TextFormField(
+            controller: _controllerId,
+            decoration: const InputDecoration(
+              icon: Icon(Icons.person),
+              hintText: "Entrez votre identifiant : ",
+            ),
+          ),
+          TextFormField(
+            controller: _controllerPassword,
+            obscureText: _passwordHidden,
+            decoration: const InputDecoration(
+              icon: Icon(Icons.password),
+              hintText: "Entrez votre Mot de passe : ",
+            ),
+            // onChanged: (_controllerPassword) {
+            //   print(_controllerPassword);
+            // },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              SerializeLogs logs = SerializeLogs(_controllerId.text, _controllerPassword.text);
+              String jsonString = jsonEncode(logs.toJson());
+              print(jsonString);
+            },
+            child: const Text("Connecter"),
+          ),
+        ]),
       ),
     );
   }
