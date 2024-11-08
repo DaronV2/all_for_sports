4652e3dcc7ea89e51c0c921e478a600d3b8ffe7c
Mahiddine3012 <Mahiddine.BenrezgaPro@outlook.fr>
2024-11-08 11:44:27 +0100

    Commentaire du code

--- lib/Screens/WareHouseSelectionScreen.dart
+++ lib/Screens/WareHouseSelectionScreen.dart
@@ -3,15 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:all_for_sports/Screens/WelcomeScreen.dart';
 
+/// Écran permettant la sélection d'un entrepôt.
+/// Affiche une liste déroulante des entrepôts disponibles avec leurs coordonnées GPS et le nom de la ville.
 class WareHouseSelectionScreen extends StatelessWidget {
+  // Liste des entrepôts disponibles avec leurs coordonnées GPS et la ville.
   final List<Map<String, dynamic>> warehouses = [
-    {"latitude": 50.3592, "longitude": 3.5253, "city": "Valenciennes"}, //TODO changer Valenciennes par Lyon
+    {
+      "latitude": 50.3592,
+      "longitude": 3.5253,
+      "city": "Valenciennes"
+    }, //TODO changer Valenciennes par Lyon
     {"latitude": 49.4944, "longitude": 0.1079, "city": "Le Havre"},
     {"latitude": 43.2965, "longitude": 5.3698, "city": "Marseille"},
-  ];                                                                // Coordonnées GPS des villes où il y a des entrepots
+  ]; // Coordonnées GPS des villes où il y a des entrepots
 
+  /// Méthode `build` pour construire l'interface utilisateur de la page de sélection d'entrepôt.
+  /// Prend un `BuildContext` comme paramètre et retourne un widget `Scaffold`.
   @override
   Widget build(BuildContext context) {
+    // Récupère le nom de l'entrepôt sélectionné via le provider WareHouseProvider.
     String selectedWarehouse = context.watch<WareHouseProvider>().entrepot;
 
     return Scaffold(
@@ -20,21 +30,28 @@ class WareHouseSelectionScreen extends StatelessWidget {
       ),
       body: Center(
         child: DropdownButton<String>(
+          // Texte affiché quand aucun entrepôt n'est sélectionné
           hint: Text('Sélectionnez un entrepôt'),
+          // Valeur sélectionnée. Null si aucun entrepôt n'a encore été choisi
           value: selectedWarehouse.isNotEmpty ? selectedWarehouse : null,
+          // Callback appelé lorsque l'utilisateur sélectionne un nouvel entrepôt
           onChanged: (String? newValue) {
             if (newValue != null) {
+              // Mise à jour de l'entrepôt sélectionné dans le provider
               context.read<WareHouseProvider>().setEntrepot(newValue);
 
-              // Redirection vers la page AccDart après la sélection
+              // Navigation vers la page d'accueil après sélection
               Navigator.pushReplacement(
                 context,
                 MaterialPageRoute(builder: (context) => WelcomeScreen()),
               );
             }
           },
+
+          // Création de la liste des éléments du menu déroulant avec les noms des villes d'entrepôts
           items: warehouses.map((warehouse) {
             return DropdownMenuItem<String>(
+              // Nom de la ville sélectionnée comme valeur
               value: warehouse['city'],
               child: Text(warehouse['city']),
             );
--- lib/Screens/WelcomeScreen.dart
+++ lib/Screens/WelcomeScreen.dart
@@ -2,12 +2,19 @@ import 'package:flutter/material.dart';
 import 'FlashQRCodeScreen.dart';
 import 'ProductListScreen.dart';
 
+/// Classe représentant la page d'accueil de l'application.
+/// Elle propose deux boutons permettant de naviguer vers la liste des produits ou vers la page de scan de QR code.
 class WelcomeScreen extends StatelessWidget {
+  // Constructeur de la classe WelcomeScreen.
+  // Paramètre optionnel `key` utilisé pour identifier de manière unique le widget dans l'arbre des widgets.
   const WelcomeScreen({Key? key}) : super(key: key);
 
+  /// Méthode principale pour construire l'interface utilisateur de la page d'accueil.
+  /// Retourne un widget `Scaffold` contenant l'AppBar et deux boutons de navigation.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Barre d'application affichant le titre de la page.
       appBar: AppBar(
         title: const Text('Page d\'Accueil - AccDart'),
       ),
@@ -15,9 +22,12 @@ class WelcomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Premier bouton pour naviguer vers la liste des produits.
             ElevatedButton(
               onPressed: () {
-                // Navigation vers la Page Ajouter Produit
+                // Navigation vers la page `ProductListScreen`.
+                // Retourne une instance de `ProductListScreen`.
+
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => ProductListScreen()),
