2ab8d21f8164dbdfc8b335b6589c8353d5a4a133
Yghermaoui <youcef18.ghermaoui@gmail.com>
2024-11-08 11:46:44 +0100

    documentationcode

--- lib/Screens/FlashQRCodeScreen.dart
+++ lib/Screens/FlashQRCodeScreen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
+/// Classe `FlashQRCodeScreen`
+///
+/// Cette classe est un widget `StatefulWidget` qui représente l'écran
+/// permettant de scanner un QR code et d'afficher le résultat.
 class FlashQRCodeScreen extends StatefulWidget {
   const FlashQRCodeScreen({super.key});
 
@@ -8,28 +12,59 @@ class FlashQRCodeScreen extends StatefulWidget {
   State<FlashQRCodeScreen> createState() => _FlashQRCodeScreenState();
 }
 
+/// Classe `_FlashQRCodeScreenState`
+///
+/// Cette classe représente l'état de `FlashQRCodeScreen` et contient
+/// la logique pour scanner un QR code et afficher le résultat.
 class _FlashQRCodeScreenState extends State<FlashQRCodeScreen> {
+  // Variable pour stocker le résultat du scan QR code
   String? scanResult;
 
+  /// Méthode `build` :
+  ///
+  /// Cette méthode construit l'interface utilisateur du scanner de QR code,
+  /// avec une caméra pour scanner les QR codes et un affichage du résultat du scan.
+  ///
+  /// Paramètre :
+  /// - `context`: le contexte de construction du widget.
+  ///
+  /// Retourne :
+  /// - Un widget `Scaffold` contenant l'interface complète.
   @override
   Widget build(BuildContext context) {
+    // Variable locale pour stocker temporairement la référence du produit scanné
     String? refProduitQrCode = '';
+
     return Scaffold(
       appBar: AppBar(
+        // Titre de l'application dans la barre supérieure
         title: const Text('Scanner un produit'),
       ),
       body: Column(
         children: <Widget>[
           Expanded(
+            // Widget prenant 5/6 de l'espace vertical pour afficher la caméra du scanner
             flex: 5,
             child: MobileScanner(
+              /// Méthode `onDetect` :
+              ///
+              /// Cette méthode est déclenchée lorsqu'un ou plusieurs QR codes sont détectés.
+              /// Elle utilise `BarcodeCapture` pour récupérer les informations du QR code scanné.
+              /// Le résultat est ensuite mis à jour dans l'interface en appelant `setState`.
+              ///
+              /// Paramètre :
+              /// - `barcodeCapture`: un objet `BarcodeCapture` contenant les informations
+              ///   des QR codes détectés.
               onDetect: (BarcodeCapture barcodeCapture) {
+                // Liste des codes barres détectés
                 final List<Barcode> barcodes = barcodeCapture.barcodes;
                 for (final barcode in barcodes) {
                   setState(() {
-                    scanResult = barcode
-                        .rawValue; // Utiliser rawValue pour obtenir la valeur du QR code
+                    // `scanResult` reçoit la valeur brute (rawValue) du QR code détecté
+                    scanResult = barcode.rawValue;
+                    // On met également à jour `refProduitQrCode` avec le résultat du scan
                     refProduitQrCode = scanResult;
+                    // Affichage dans la console de la référence du produit
                     print(refProduitQrCode);
                   });
                 }
@@ -37,12 +72,14 @@ class _FlashQRCodeScreenState extends State<FlashQRCodeScreen> {
             ),
           ),
           Expanded(
+            // Widget prenant 1/6 de l'espace vertical pour afficher le texte du résultat
             flex: 1,
             child: Center(
+              // Affiche le résultat du scan s'il est disponible, sinon un message par défaut
               child: Text(
                 scanResult != null
                     ? 'Résultat du scan: $scanResult'
-                    : 'Scannez un QR code',
+                    : 'Scannez un QR code', // Message d'instruction par défaut
               ),
             ),
           ),
--- lib/Screens/ProductListScreen.dart
+++ lib/Screens/ProductListScreen.dart
@@ -1,8 +1,11 @@
-import 'package:all_for_sports/Screens/AddProductScreen.dart';
 import 'package:all_for_sports/Services/FillProductList.dart';
 import 'package:flutter/material.dart';
 import 'package:all_for_sports/Widget/ProductItem.dart';
 
+/// Classe `ProductListScreen`
+///
+/// Ce widget représente l'écran affichant une liste de produits.
+/// Il utilise une source de données pour charger les informations des produits.
 class ProductListScreen extends StatefulWidget {
   ProductListScreen({super.key});
 
@@ -10,60 +13,71 @@ class ProductListScreen extends StatefulWidget {
   State<ProductListScreen> createState() => _ProductListScreen();
 }
 
+/// Classe `_ProductListScreen`
+///
+/// Cette classe gère l'état de l'écran de liste des produits.
+/// Elle contient la logique pour charger les données des produits et les afficher dans une liste.
 class _ProductListScreen extends State<ProductListScreen> {
+  // Liste pour stocker les widgets `ProductItem` représentant chaque produit
   List<ProductItem> listeProd = [];
 
+  /// Méthode `initState` :
+  ///
+  /// Cette méthode est appelée automatiquement lors de l'initialisation de l'état du widget.
+  /// Elle initialise la liste des produits en appelant `loadList`.
   @override
   void initState() {
     super.initState();
-    loadList();
+    loadList(); // Appel de la fonction pour charger la liste de produits
   }
 
+  /// Méthode `loadList` :
+  ///
+  /// Cette méthode charge les données des produits en appelant une méthode asynchrone
+  /// `FillProductList.loadProductList()`. Elle met à jour l'interface une fois les données chargées.
+  ///
+  /// Retourne :
+  /// - Un `Future<void>`, car la méthode est asynchrone.
   Future<void> loadList() async {
+    // Charge la liste des produits et l'assigne à `listeProd`
     listeProd = await FillProductList.loadProductList();
-    print("los datas ");
+    print("los datas "); // Message de vérification dans la console
+
+    // Affiche chaque produit dans la console pour vérification
     listeProd.forEach((data) {
       print(data);
     });
+
+    // Rafraîchit l'interface pour afficher les nouveaux produits chargés
     setState(() {});
   }
 
+  /// Méthode `build` :
+  ///
+  /// Cette méthode construit l'interface utilisateur, qui contient une liste de produits.
+  ///
+  /// Paramètre :
+  /// - `context`: le contexte de construction du widget.
+  ///
+  /// Retourne :
+  /// - Un widget `Scaffold` contenant l'interface de la liste des produits.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // Titre dans la barre supérieure
         title: const Text('Liste des produits'),
       ),
       body: Center(
         child: Stack(
           children: [
-            // Utilisation de ListView.builder pour le défilement infini
+            // Utilisation de ListView.builder pour afficher et défiler la liste des produits
             ListView.builder(
-              itemCount: listeProd.length,
+              itemCount: listeProd.length, // Nombre d'éléments dans la liste
               itemBuilder: (context, index) {
-                return listeProd[index]; // Affiche chaque produit
+                // Retourne chaque widget `ProductItem` de la liste
+                return listeProd[index];
               },
-            ),
-            Positioned(
-              bottom: 20.0,
-              right: 20.0,
-              child: FloatingActionButton(
-                onPressed: () {
-                  // Ouvrir l'écran d'ajout de produit
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          const AddProductScreen(),
-                    ),
-                  );
-                },
-                child: const Icon(
-                  Icons.add,
-                  size: 40, // Taille de l'icône plus grande
-                ),
-                backgroundColor: Colors.blue,
-                tooltip: 'Ajouter un produit',
-              ),
             ),
           ],
         ),
